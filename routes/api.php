9ed37eb66c85a7c501e3b99705fda1ec9e93b442
KamilleWudes <judegbone@kimoasoft.com>
2024-08-09 17:58:47 +0200

    Set up a fresh Laravel app

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Helpers\Routes\RouteHelper;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function (): void {
+
+    RouteHelper::includeRouteFiles(directory: __DIR__.'/api/v1');
+});
